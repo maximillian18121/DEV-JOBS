158782b3543ce3386a753c326d7938fc6aa86b58
JAYESH <jayesh.chauhan@cubastion.com>
2026-04-06 12:24:58 +0530

    Initial Project setup
    [1]: initializing the npm.
    [2]: installing all the important libraries -> nodemon, express, sequelize.
    [3]: running sequelize init query for creating a sequelize mrc backend broilerplate.
    [4] creating the server and running it on the port.
    [5] Using dotenv library for keeping env secret private to the localhost

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.DB_PORT, ()=>{
+    console.log("Server is running on port 5500");
+})
+
